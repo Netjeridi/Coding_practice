8ebfaef157114390243156efa30a05ffff704fc6
Netjeridi <43844139+Netjeridi@users.noreply.github.com>
2022-01-20 18:13:55 +0000

    added csv storage for html form

--- web_development/web_server/server.py
+++ web_development/web_server/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from datetime import date
+import csv
 
 app = Flask(__name__)
 
@@ -16,10 +18,31 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f"\n\nDate: {date.today()}\nEmail: {email}\nSubject: {subject}\nMessage: {message}")
+    return True
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='',  mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([date.today(), email, subject, message])
+    return True
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
-        return redirect('submitted.html')
-    else:
-        return "Something went wrong. Data not submitted."
+        success = write_to_csv(data)
+        if success:
+            return redirect('submitted.html')
+        else:
+            return "Something went wrong. Data not submitted."
